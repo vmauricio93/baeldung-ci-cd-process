9238eaca602ce583932da6d340ae37a68737429b
Mauro Vargas <vmauricio823@hotmail.com>
2020-07-14 10:53:41 -0500

    Initialize Spring App

---
+++ cicd/src/test/java/com/spring/cicd/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
